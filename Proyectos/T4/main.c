dbc846622befa471129fc966c22fe6e0b8695a86
AlexLlenas <alex.llenas.arias@gmail.com>
2024-02-21 18:36:42 -0400

    initial commit

---
+++ Proyectos/T4/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Hello \n");
+	FILE *dataFile = fopen("data.csv","r");
+	if (dataFile == NULL){
+		printf("Error abriendo el programa\n");
+		exit(-1);
+	}
+	printf("Archivo leido \n");
+	return 0;
+}
